c7a2bedbc9129c53405d907fc34be6dcdf3a1da2
dina ammam <dina.ammam34@gmail.com>
2024-03-09 01:21:22 +0200

    linq

---
+++ linq/linq lap/linq/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace linq
+{
+    public static class operations
+    {
+        public static int sum(this int num1 , int num2 ) => num1 + num2;
+        public static int sub(this int num1, int num2) => num1 - num2;
+        public static int mul(this int num1, int num2) => num1 * num2;
+        public static int div(this int num1, int num2) => num1 / num2;
+
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            int sum = a.sum(b);
+            int mul = a.mul(b);
+
+            Console.WriteLine(  $"sum:{ sum}");
+            Console.WriteLine($"mul:{mul}");
+        }
+    }
+}
