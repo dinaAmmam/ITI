2894324c938d3a82d30008f284399bc3a1c48904
dina ammam <dina.ammam34@gmail.com>
2024-03-23 23:58:37 +0200

    mvc lap1

---
+++ mvc/lap1/lap1/lap1/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lap1.Controllers
+{
+    public class StudentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Display()
+        {
+            return "first lap mvc";
+        }
+        public int Add(int x , int y)
+        {
+            return x + y;
+        }
+    }
+}
