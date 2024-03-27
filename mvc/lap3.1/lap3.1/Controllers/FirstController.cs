d54ea1e3913c85f1f57fce5ab66d601d760cc467
dina ammam <dina.ammam34@gmail.com>
2024-03-27 07:29:44 +0200

    lap3

---
+++ mvc/lap3.1/lap3.1/Controllers/FirstController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lap3._1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Add(Dictionary<int , int> degree) {
+            return View();
+
+        }
+    }
+}
---
+++ mvc/lap3.1/lap3.1/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lap3._1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Add(int id, string name, int age, IFormFile stdimg)
+        {
+            string fileext = stdimg.FileName.Split('.').Last();
+            //var fs = new FileStream($"wwwroot/images/a1.{fileext}" , FileMode.Create);
+            //await stdimg.CopyToAsync(fs);
+            //return Content("added");
+            //fs.Dispose();
+            using( var fs = new FileStream($"wwwroot/images/a1.{fileext}", FileMode.Create))
+            {
+                await stdimg.CopyToAsync(fs);
+
+            }
+            return Content("added");
+
+        }
+    }
+}
